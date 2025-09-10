00d2ed1a6a690b64993d69ad9119c5ec1032cf01
ogata touya <ogata11260@icloud.com>
2025-09-11 05:40:51 +0900

    contact-form3 の内容を反映

---
+++ src/app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthenticatedSessionController extends Controller
+{
+    /**
+     * ログアウト処理
+     */
+    public function destroy(Request $request)
+    {
+        Auth::guard('web')->logout();
+
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+
+        return redirect('/login'); // ログアウト後にログイン画面へ
+    }
+}
