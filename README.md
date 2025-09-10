213d1864f90fc329c3c30b809735863cea8001c1
ogata touya <ogata11260@icloud.com>
2025-09-11 05:13:46 +0900

    お問い合わせフォーム作成

--- README.md
+++ README.md
@@ -1,1 +1,54 @@
-# laravel-docker-template
+# laravel-docker-templat
+
+# お問合せフォーム（アプリ名）
+
+## 概要
+練習用のお問合せフォームです
+
+## 環境構築手順
+
+リポジトリをClone
+
+git clone git@github.com:ogatatouya/contact-form3.git
+
+
+Dockerを起動
+
+docker compose up -d --build
+
+
+envファイルの準備
+
+cp src/.env.example src/.env
+
+envファイルの書き換えが必要
+
+DB_USERNAME=laravel_user
+
+DB_PASSWORD=laravel_pass
+
+
+Laravelのセットアップ
+
+docker-compose exec php bash
+
+composer install
+
+php artisan key:generate
+php artisan migrate
+
+
+
+## 使用技術(実行環境)
+- PHP 8.1
+- Laravel 8.83.8
+- MySQL 8.0
+
+## ER図
+![ER図](index.drawio.png)
+
+## 動作URL
+- 開発環境：http://localhost/
+- phpMyAdmin:：http://localhost:8080/
+
+`` `
--- src/routes/web.php
+++ src/routes/web.php
@@ -1,18 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ContactController;
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
-Route::get('/', function () {
-    return view('welcome');
+Route::middleware('auth')->group(function () {
+    Route::get('/', [ContactController::class, 'index'])->name('contact.index');
+    Route::post('/contacts/confirm', [ContactController::class, 'confirm'])->name('contact.confirm');
+    Route::post('/contacts', [ContactController::class, 'store'])->name('contact.store');
 });
+
+
+Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])->name('logout');
